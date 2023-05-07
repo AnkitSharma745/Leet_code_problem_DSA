2c490db7315242d0dfb8b8754cec076762c988ba
Ankit Sharma <ankitaksharma9763@gmail.com>
2023-05-07 23:17:52 +0530

    JavaScript code has been done for this problem

---
+++ 69. Sqrt(x)/JavaScript_code.js
@@ -0,0 +1,8 @@
+var mySqrt = function(x) {
+  let sqt = Math.sqrt(x)
+  return Math.floor(sqt)
+};
+
+// Math.sqrt(x) calculates the square root of x using the Math.sqrt() method. The result is stored in a variable named sqt.
+// Math.floor(sqt) rounds down the square root value sqt to the nearest integer value that is less than or equal to sqt.
+// The function returns the rounded-down integer value of the square root of x.
